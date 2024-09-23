aa51a7a2db0aed04521f6f897cf41cd338833a4d
Chigaemezu Dennar <salientcrossing@outlook.com>
2024-09-23 03:46:42 -0600

    cc

--- variable.tf
+++ variable.tf
@@ -3,7 +3,13 @@ variable "bucket" {
 }
 
 variable "function_name" {
-  type = string
+  description = "The name of the Lambda function"
+  type        = string
+  
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9-_]+$", var.function_name)) && length(var.function_name) >= 1 && length(var.function_name) <= 140
+    error_message = "Function name must be between 1 and 140 characters long and can only contain letters, numbers, hyphens, and underscores."
+  }
 }
 
 variable "sqs_queue" {
