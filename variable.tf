53d6aa775c3f1a4cded4f1197de59d0a735268e1
Chigaemezu Dennar <salientcrossing@outlook.com>
2024-09-23 03:58:18 -0600

    cc

--- variable.tf
+++ variable.tf
@@ -5,7 +5,8 @@ variable "bucket" {
 variable "function_name" {
   description = "The name of the Lambda function"
   type        = string
-  
+  default     = "alphas3-lambda-function-name"
+
   validation {
     condition     = can(regex("^[a-zA-Z0-9-_]+$", var.function_name)) && length(var.function_name) >= 1 && length(var.function_name) <= 140
     error_message = "Function name must be between 1 and 140 characters long and can only contain letters, numbers, hyphens, and underscores."
